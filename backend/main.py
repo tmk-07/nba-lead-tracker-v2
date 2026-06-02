2115f1fe2e5528ef7f518b9a8dbe30ff905c20e5
tmk07 <timmykim07@gmail.com>
2026-06-02 16:47:28 -0500

    temp allow all cors origins

--- backend/main.py
+++ backend/main.py
@@ -15,11 +15,7 @@ app = FastAPI(title="NBA Lead Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://nba-lead-tracker-v2.vercel.app/",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
